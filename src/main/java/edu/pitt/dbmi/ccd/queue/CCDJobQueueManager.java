73d0f9a19babcfbe5a390f458e73d8d332e492e3
chirayukong <chirayu.kong@gmail.com>
2015-08-19 09:59:42 -0400

    First commit

---
+++ src/main/java/edu/pitt/dbmi/ccd/queue/CCDJobQueueManager.java
@@ -0,0 +1,44 @@
+package edu.pitt.dbmi.ccd.queue;
+/*
+ * Copyright (C) 2015 University of Pittsburgh.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 
+ * Aug 4, 2015 3:48:15 PM
+ * 
+ * @author Chirayu (Kong) Wongchokprasitti
+ *
+ */
+@SpringBootApplication
+@EnableScheduling
+public class CCDJobQueueManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(CCDJobQueueManager.class, args);
+	}
+
+}
